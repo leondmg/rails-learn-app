2a06f7b64423eadcf641e0c972bee7e3874439c2
Dmitry Leontev <dmitriy1leontev@gmail.com>
2022-12-01 19:38:02 +0300

    Add helpers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
   
   resources :questions do
-    resources :answers, only: [:create, :destroy]
+    resources :answers, except: [:new, :show]
   end
 end
